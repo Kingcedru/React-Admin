41c5edc57ca051ce8f39bb76b4a25a3268b00065
Kingcedru <byishimocedrick@gmail.com>
2024-07-10 10:37:14 +0200

    created the categories

--- ra-app/src/App.tsx
+++ ra-app/src/App.tsx
@@ -1,14 +1,28 @@
 import jsonServerProvider from "ra-data-json-server";
 import { Admin, Resource } from "react-admin";
-import { PosterList } from "./components/PosterList";
-import { PosterEdit } from "./components/PosterEdit";
-import { PosterCreate } from "./components/PosterCreate";
+import { PosterList } from "./components//posters/PosterList";
+import { PosterEdit } from "./components/posters/PosterEdit";
+import { PosterCreate } from "./components//posters/PosterCreate";
+import { CategoryList } from "./components/categories/CategoriesList";
+import { CategoryEdit } from "./components/categories/CategoriesEdit";
+import { CategoryCreate } from "./components/categories/CategoriesCreate";
 
 function App() {
   const dataProvider = jsonServerProvider("http://localhost:3000");
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posters" list={PosterList} edit={PosterEdit} create={PosterCreate}></Resource>
+      <Resource
+        name="categories"
+        list={CategoryList}
+        edit={CategoryEdit}
+        create={CategoryCreate}
+      ></Resource>
+      <Resource
+        name="posters"
+        list={PosterList}
+        edit={PosterEdit}
+        create={PosterCreate}
+      ></Resource>
     </Admin>
   );
 }

6bd93cf027402059ce642785a6f2e1c397d68ba9
Kingcedru <byishimocedrick@gmail.com>
2024-07-10 10:16:56 +0200

    added a create operation

--- ra-app/src/App.tsx
+++ ra-app/src/App.tsx
@@ -2,12 +2,13 @@ import jsonServerProvider from "ra-data-json-server";
 import { Admin, Resource } from "react-admin";
 import { PosterList } from "./components/PosterList";
 import { PosterEdit } from "./components/PosterEdit";
+import { PosterCreate } from "./components/PosterCreate";
 
 function App() {
   const dataProvider = jsonServerProvider("http://localhost:3000");
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posters" list={PosterList} edit={PosterEdit}></Resource>
+      <Resource name="posters" list={PosterList} edit={PosterEdit} create={PosterCreate}></Resource>
     </Admin>
   );
 }

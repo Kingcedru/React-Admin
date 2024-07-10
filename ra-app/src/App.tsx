a2130d1548c46547c3712d42c0fce0104937abd4
Kingcedru <byishimocedrick@gmail.com>
2024-07-10 09:49:30 +0200

    edit the panel

--- ra-app/src/App.tsx
+++ ra-app/src/App.tsx
@@ -1,12 +1,13 @@
 import jsonServerProvider from "ra-data-json-server";
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { PosterList } from "./components/PosterList";
+import { PosterEdit } from "./components/PosterEdit";
 
 function App() {
   const dataProvider = jsonServerProvider("http://localhost:3000");
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posters" list={PosterList}></Resource>
+      <Resource name="posters" list={PosterList} edit={PosterEdit}></Resource>
     </Admin>
   );
 }
